97dc2560b1dbd2edd49452ffaa9da2f3493bbaef
richad97 <rcastillo2022@gmail.com>
2021-03-25 10:13:10 -0700

    added spacing between columns and fixed responsiveness for most part

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         $page_requested = "line_chart";
     }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,12 +15,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>D3 Assist</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
     <link rel="stylesheet" type="text/css" href="./node_modules/spectrum-colorpicker2/dist/spectrum.css">
     <link rel="stylesheet" href="./scss/main.css" />
-
+  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-
     <!--<script type="text/javascript" src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>-->
     <script type="text/javascript" src="./node_modules/jquery/dist/jquery.min.js"></script>
     <script type="text/javascript" src="./node_modules/spectrum-colorpicker2/dist/spectrum.min.js"></script>
@@ -34,7 +33,6 @@
         window.alert("Code has been copied to your clipboard.");
       }
     </script>
-
     <!--<script type="module" src="./src/<?php echo $page_requested; ?>/<?php echo $page_requested; ?>_main.js"></script>-->
   </head>
 
@@ -79,4 +77,5 @@
     </body>
 
     <script src="./dist/bundle.js"></script>
+    
 </html>
