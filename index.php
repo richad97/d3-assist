ec3b3081aafa11c441fd81fd2cdcfe82b12045d4
richad97 <rcastillo2022@gmail.com>
2021-03-26 13:28:59 -0700

    made form collapsible with jquery

--- index.php
+++ index.php
@@ -17,7 +17,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./node_modules/spectrum-colorpicker2/dist/spectrum.css">
     <link rel="stylesheet" href="./scss/main.css" />
-  
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <!--<script type="text/javascript" src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>-->
     <script type="text/javascript" src="./node_modules/jquery/dist/jquery.min.js"></script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,63 @@
 import { initLineChart } from "./line_chart/line_chart_main.js";
 //import { initBarChart } from "./bar_chart/bar_chart_main.js";
 
-initLineChart();
+$(document).ready(function () {
+  initLineChart();
+
+  $("#title_arrow_right_icon").hide();
+  $("#slider_arrow_right_icon").hide();
+  $("#checkbox_arrow_right_icon").hide();
+  $("#color_arrow_right_icon").hide();
+
+  $("#title_arrow_down_icon").click(function () {
+    $(".line_title_col_row").slideToggle("slow");
+
+    $(this).hide();
+    $("#title_arrow_right_icon").show();
+  });
+  $("#title_arrow_right_icon").click(function () {
+    $(".line_title_col_row").slideToggle("slow");
+
+    $(this).hide();
+    $("#title_arrow_down_icon").show();
+  });
+
+  $("#slider_arrow_down_icon").click(function () {
+    $(".line_slider_col_row").slideToggle("slow");
+
+    $(this).hide();
+    $("#slider_arrow_right_icon").show();
+  });
+  $("#slider_arrow_right_icon").click(function () {
+    $(".line_slider_col_row").slideToggle("slow");
+
+    $(this).hide();
+    $("#slider_arrow_down_icon").show();
+  });
+
+  $("#checkbox_arrow_down_icon").click(function () {
+    $(".line_checkbox_col_row").slideToggle("slow");
+
+    $(this).hide();
+    $("#checkbox_arrow_right_icon").show();
+  });
+  $("#checkbox_arrow_right_icon").click(function () {
+    $(".line_checkbox_col_row").slideToggle("slow");
+
+    $(this).hide();
+    $("#checkbox_arrow_down_icon").show();
+  });
+
+  $("#color_arrow_down_icon").click(function () {
+    $(".line_color_col_row").slideToggle("slow");
+
+    $(this).hide();
+    $("#color_arrow_right_icon").show();
+  });
+  $("#color_arrow_right_icon").click(function () {
+    $(".line_color_col_row").slideToggle("slow");
+
+    $(this).hide();
+    $("#color_arrow_down_icon").show();
+  });
+});
